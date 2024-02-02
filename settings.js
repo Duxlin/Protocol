7bfb213ef3243af36f360ac060209d9fda1a7c91
Duxlin <folorunshoabdulquodir1@gmail.com>
2024-02-02 21:42:40 +0100

    non

---
+++ settings.js
@@ -0,0 +1,61 @@
+//base by DGXeon
+//re-upload? recode? copy code? give credit ya :)
+//YouTube: @DGXeon
+//Instagram: unicorn_xeon13
+//Telegram: t.me/xeonbotinc
+//GitHub: @DGXeon
+//WhatsApp: +916909137213
+//want more free bot scripts? subscribe to my youtube channel: https://youtube.com/@DGXeon
+
+const fs = require('fs')
+const chalk = require('chalk')
+
+//owmner v card
+global.ytname = "YT: Duxbot6413" //ur yt chanel name
+global.socialm = "GitHub: Duxlin" //ur github or insta name
+global.location = "Alien Realm" //ur location
+
+//new
+global.botname = '𝐃𝐔𝐗-𝐌𝐃' //ur bot name
+global.ownernumber = '2347082252014' //ur owner number
+global.ownername = '𝚀𝚞𝚊𝚍𝚑𝚒𝚛' //ur owner name
+global.websitex = "https://tiktok.com/@quadhir"
+global.wagc = "https://chat.whatsapp.com/BZK6e00lnLL5t4aH9NfU4n"
+global.themeemoji = '🌝'
+global.wm = "Dux Bot Inc."
+global.botscript = 'https://github.com/Duxlin' //script link
+global.packname = "Sticker By"
+global.author = " 𝙸𝚖𝚖𝚘𝚛𝚝𝚊𝚕𝙳𝚞𝚡\n\n+2347082252014"
+global.creator = "2347082252014@s.whatsapp.net"
+global.xprefix = '.'
+global.premium = ["2347082252014"] // Premium User
+global.hituet = 0
+
+//bot sett
+global.typemenu = '𝙳𝚞𝚡' // menu type 'v1' => 'v8'
+global.typereply = '𝙳𝚞𝚡-𝚖𝚍' // reply type 'v1' => 'v3'
+global.autoblocknumber = '92' //set autoblock country code
+global.antiforeignnumber = '91' //set anti foreign number country code
+global.welcome = true //welcome/left in groups
+global.anticall = false //bot blocks user when called
+global.autoswview = true //auto status/story view
+global.adminevent = false //show promote/demote message
+global.groupevent = false //show update messages in group chat
+//msg
+global.mess = {
+	limit: 'Your limit is up!',
+	nsfw: 'Nsfw is disabled in this group, Please tell the admin to enable',
+    done: 'Done✓',
+    error: 'Error!',
+    success: 'Here you go!'
+}
+//thumbnail
+global.thumb = fs.readFileSync('./XeonMedia/theme/cheemspic.jpg')
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+    fs.unwatchFile(file)
+    console.log(chalk.redBright(`Update'${__filename}'`))
+    delete require.cache[file]
+    require(file)
+})
