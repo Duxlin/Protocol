9c45494f5cb22447de74aa43d721de932b8e6102
Duxlin <96833294+Duxlin@users.noreply.github.com>
2024-03-05 00:07:01 +0100

    Add files via upload

--- settings.js
+++ settings.js
@@ -10,38 +10,36 @@
 const fs = require('fs')
 const chalk = require('chalk')
 
-//session
-global.sessionid ='ur session id'
-
 //owmner v card
-global.ytname = "YT: Duxlin" //ur yt chanel name
+global.ytname = "YT: Duxbot6413" //ur yt chanel name
 global.socialm = "GitHub: Duxlin" //ur github or insta name
-global.location = "Alien realm" //ur location
+global.location = "Alien Realm" //ur location
 
 //new
-global.botname = 'Dux Bot MD' //ur bot name
+global.botname = '𝐃𝐔𝐗-𝐌𝐃' //ur bot name
 global.ownernumber = '2347082252014' //ur owner number
-global.ownername = 'Dux' //ur owner name
-global.websitex = "https://www.tiktok.com/@quadhir"
-global.wagc = ""
-global.themeemoji = '😊'
+global.ownername = '𝚀𝚞𝚊𝚍𝚑𝚒𝚛' //ur owner name
+global.websitex = "https://tiktok.com/@quadhir"
+global.wagc = "https://chat.whatsapp.com/BZK6e00lnLL5t4aH9NfU4n"
+global.themeemoji = '🌝'
 global.wm = "Dux Bot Inc."
 global.botscript = 'https://github.com/Duxlin' //script link
-global.packname = "Dux"
-global.author = "Dux\n\n+2347082252014"
+global.packname = "Sticker By"
+global.author = " 𝙸𝚖𝚖𝚘𝚛𝚝𝚊𝚕𝙳𝚞𝚡\n\n+2347082252014"
 global.creator = "2347082252014@s.whatsapp.net"
 global.xprefix = '.'
-global.premium = ["2347082252014","2349167632397"] // Premium User
+global.premium = ["2347082252014"] // Premium User
 global.hituet = 0
 
+//bot sett
 //bot sett
 global.typemenu = 'v8' // menu type 'v1' => 'v8'
 global.typereply = 'v2' // reply type 'v1' => 'v3'
 global.autoblocknumber = '92' //set autoblock country code
 global.antiforeignnumber = '91' //set anti foreign number country code
-global.welcome = false //welcome/left in groups
+global.welcome = true //welcome/left in groups
 global.anticall = false //bot blocks user when called
-global.autoswview = false //auto status/story view
+global.autoswview = true //auto status/story view
 global.adminevent = false //show promote/demote message
 global.groupevent = false //show update messages in group chat
 //msg
