ee863ac54c3b8d93ea1bba76d17d4a12857e289b
Duxlin <96833294+Duxlin@users.noreply.github.com>
2024-06-24 23:07:14 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -32,8 +32,8 @@ global.premium = ["2347082252014","2349167632397"] // Premium User
 global.hituet = 0
 
 //bot sett
-global.typemenu = 'v12' // menu type 'v1' => 'v12'
-global.typereply = 'v4' // reply type 'v1' => 'v4'
+global.typemenu = 'v2' // menu type 'v1' => 'v12'
+global.typereply = 'v2' // reply type 'v1' => 'v4'
 global.autoblocknumber = '92' //set autoblock country code
 global.antiforeignnumber = '91' //set anti foreign number country code
 global.welcome = false //welcome/left in groups
